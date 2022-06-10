d9f398362b6dedd33ee7186c94e21be072860cb0
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-06-10 00:14:40 -0400

    color chagnes

--- src/components/Search.js
+++ src/components/Search.js
@@ -35,71 +35,38 @@ const Search = ({ searchTerm, onChangeSearch, setStatus, status }) => {
       />
 
       <Dropdown.Toggle
-        className="text-white"
+        className={`${status} text-uppercase `}
         variant="danger"
         id="dropdown-split-basic"
       >
         {capitalizeFirstLetter(status) + " "}
       </Dropdown.Toggle>
 
-      <Dropdown.Menu className="bg-danger opacity-75" onClick={statusHandler}>
-        <Dropdown.Item className="text-white" value="all">
+      <Dropdown.Menu
+        className="bg-danger text-uppercase font-weight-bold opacity-75 "
+        onClick={statusHandler}
+      >
+        <Dropdown.Item className="text-white " value="all">
           All
         </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="bug">
-          Bug
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="dark">
-          Dark
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="dragon">
-          Dragon
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="electric">
-          Electric
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="fairy">
-          Fairy
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="Fighting">
-          Fighting
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="fire">
-          Fire
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="flying">
-          Flying
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="ghost">
-          Ghost
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="grass">
-          Grass
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="ground">
-          Ground
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="ice">
-          Ice
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="normal">
-          Normal
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="poison">
-          Poison
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="psychic">
-          Psychic
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="rock">
-          Rock
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="steel">
-          Steel
-        </Dropdown.Item>
-        <Dropdown.Item className="text-white" value="water">
-          Water
-        </Dropdown.Item>
+        <Dropdown.Item className="bug-text ">Bug</Dropdown.Item>
+        <Dropdown.Item className="dark-text">Dark</Dropdown.Item>
+        <Dropdown.Item className="dragon-text">Dragon</Dropdown.Item>
+        <Dropdown.Item className="electric-text">Electric</Dropdown.Item>
+        <Dropdown.Item className="fairy-text">Fairy</Dropdown.Item>
+        <Dropdown.Item className="Fighting-text">Fighting</Dropdown.Item>
+        <Dropdown.Item className="fire-text">Fire</Dropdown.Item>
+        <Dropdown.Item className="flying-text">Flying</Dropdown.Item>
+        <Dropdown.Item className="ghost-text">Ghost</Dropdown.Item>
+        <Dropdown.Item className="grass-text">Grass</Dropdown.Item>
+        <Dropdown.Item className="ground-text">Ground</Dropdown.Item>
+        <Dropdown.Item className="ice-text">Ice</Dropdown.Item>
+        <Dropdown.Item className="normal-text">Normal</Dropdown.Item>
+        <Dropdown.Item className="poison-text">Poison</Dropdown.Item>
+        <Dropdown.Item className="psychic-text">Psychic</Dropdown.Item>
+        <Dropdown.Item className="rock-text">Rock</Dropdown.Item>
+        <Dropdown.Item className="steel-text">Steel</Dropdown.Item>
+        <Dropdown.Item className="water-text">Water</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
